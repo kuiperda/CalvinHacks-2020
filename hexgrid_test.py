ce6cc617f4a6290d3d9795ab746ad2e81911b8f7
cmd16 <catherine.dejager10@gmail.com>
2020-02-22 03:13:17 -0500

    draw hexes by direction

--- hexgrid_test.py
+++ hexgrid_test.py
@@ -11,38 +11,41 @@ def main():
     pygame.init()
     pygame.display.set_caption("hexgrid test")
 
+    hex_image = pygame.image.load("HexMap/regular_hex.png")
+
     hex_width = 338
     width_scale = 0.86
     height_scale = 0.5
     x_dist = hex_width * width_scale
-    y_dist = hex_width
+    y_dist = hex_width * height_scale
 
     # hex_directions = [Hex(1, 0, -1), Hex(1, -1, 0), Hex(0, -1, 1), Hex(-1, 0, 1), Hex(-1, 1, 0), Hex(0, 1, -1)]
     # maps direction to change in x and y
-    dir_dict = {(1, 0, -1): (hex_width * width_scale, -hex_width * height_scale),
-                (1, -1, 0): (hex_width * width_scale, -height_scale),
-                (0, -1, 1): (0, hex_width * height_scale),
+    dir_dict = {(1, 0, -1): (x_dist, -y_dist),
+                (1, -1, 0): (x_dist, y_dist),
+                (0, -1, 1): (0, y_dist * 2),
+                (-1, 0, 1): (-x_dist, y_dist),
+                (-1, 1, 0): (-x_dist, -y_dist),
+                (0, 1, -1): (0, -y_dist*2)
                 }
     dirs = list(dir_dict.keys())
 
     screen = pygame.display.set_mode((1000, 1000))
     screen.fill((255, 255, 255))
 
-    hex_image = pygame.image.load("HexMap/regular_hex.png")
-
     x = 200
     y = 200
 
-    # screen.blit(hex_image, (x, y))
-    # for dir in dirs[:2]:
-    #     delta_x, delta_y = dir_dict[dir]
-    #     screen.blit(hex_image, (x + delta_x, y + delta_y))
+    screen.blit(hex_image, (x, y))
+    for dir in dirs:
+        delta_x, delta_y = dir_dict[dir]
+        screen.blit(hex_image, (x + delta_x, y + delta_y))
 
-    screen.blit(hex_image, (0, hex_width * height_scale))
+    # screen.blit(hex_image, (0, hex_width * height_scale))
 
-    screen.blit(hex_image, (hex_width * width_scale, 0))
-    screen.blit(hex_image, (hex_width * width_scale, hex_width))
-    screen.blit(hex_image, (0, hex_width * (1 + height_scale)))
+    # screen.blit(hex_image, (hex_width * width_scale, 0))
+    # screen.blit(hex_image, (hex_width * width_scale, hex_width))
+    # screen.blit(hex_image, (0, hex_width * height_scale + hex_width))
 
     pygame.display.flip()
 
