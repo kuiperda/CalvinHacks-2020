cb4680c2be65cd85b2f9eb6077eb404ed027fa6c
Josh Wright <wright.jjw@gmail.com>
2020-02-22 02:32:05 -0500

    TurnTimer.drawBar()

--- src/TurnTimer.py
+++ src/TurnTimer.py
@@ -6,13 +6,26 @@ For CalvinHacks 2020
 
 INTERVAL = 10   # Length of turn in frames
 
+import pygame
+
 class TurnTimer:
-    def __init__(self):
+    """
+    Constructor.
+    Takes width and height params as the size of the bar.
+    """
+    def __init__(self, width, height):
         self._interval = INTERVAL
         self._current_tick = 0
+        self._width = width
+        self._height = height
+        self._surface = pygame.Surface((width,height))
+        self._border_rect = pygame.Rect(0, 0, self._width, self._height)
+        self._fill_rect = pygame.Rect(5, 5, self._width - 10, self._height - 10)
+
+        self.drawBar()
     
     """
-    Incraese the tick timer.
+    Increase the tick timer.
     Should be called on every frame.
     """
     def tick(self):
@@ -20,3 +33,11 @@ class TurnTimer:
         if self._interaval == self._current_tick:
             # TODO: Throw an event or something
             self._current_tick = 0
+
+    """
+    Draw the empty bar.
+    Should not be called by an external function.
+    """
+    def drawBar(self):
+        pygame.draw.rect(self._surface, "white", self._border_rect)
+        pygame.draw.rect(self._surface, "black", self._fill_rect)
