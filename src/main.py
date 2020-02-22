532a3ea3ced647f619fc959ba2773a0ea65563e2
Josh Wright <wright.jjw@gmail.com>
2020-02-21 19:52:36 -0500

    clock and screen updates

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ import pygame
 screen = pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("Caster Blaster")
 
+clock = pygame.time.Clock()
+
 # The main loop
 def mainLoop():
     while True:
@@ -18,5 +20,8 @@ def mainLoop():
         for event in eventList:
             if event.type == pygame.QUIT:
                 return
+
+    pygame.display.flip()   # update the screen
+    clock.tick()            # tick the clock
 
 mainLoop()
