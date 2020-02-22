efce4c1d53984461115d85779b28866cbc5bf456
Josh Wright <wright.jjw@gmail.com>
2020-02-21 19:37:41 -0500

    events

--- src/main.py
+++ src/main.py
@@ -7,9 +7,16 @@ For CalvinHacks 2020
 # Initialize pygame
 import pygame
 
-screen = pygame.display.set_mode((700,700))
+screen = pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("Caster Blaster")
 
 # The main loop
-while True:
-    
+def mainLoop():
+    while True:
+        eventList = pygame.event.get()
+        
+        for event in eventList:
+            if event.type == pygame.QUIT:
+                return
+
+mainLoop()
