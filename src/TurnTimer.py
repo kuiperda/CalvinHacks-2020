5cfa29ac083248785408762997509463d4505302
Josh Wright <wright.jjw@gmail.com>
2020-02-22 00:43:30 -0500

    this -> self + doc

--- src/TurnTimer.py
+++ src/TurnTimer.py
@@ -8,11 +8,15 @@ INTERVAL = 10   # Length of turn in frames
 
 class TurnTimer:
     def __init__(self):
-        this._interval = INTERVAL
-        this._current_tick = 0
+        self._interval = INTERVAL
+        self._current_tick = 0
     
+    """
+    Incraese the tick timer.
+    Should be called on every frame.
+    """
     def tick(self):
-        this._current_tick += 1
-        if this._interaval == this._current_tick:
+        self._current_tick += 1
+        if self._interaval == self._current_tick:
             # TODO: Throw an event or something
-            this._current_tick = 0
+            self._current_tick = 0
